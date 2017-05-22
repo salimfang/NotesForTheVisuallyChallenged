f63b4ce5b3b2855fb689659b19ff9e521e477735
salimfang <tiamero810@gmail.com>
2017-05-22 17:26:28 +0800

    first update

---
+++ AudioNotes/NoteCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NoteCollectionViewCell.swift
+//  AudioNotes
+//
+//  Created by Salim Fang on 2017/5/22.
+//  Copyright © 2017年 MAP. All rights reserved.
+//
+
+import UIKit
+
+class NoteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tagShown: UILabel!
+    
+}
